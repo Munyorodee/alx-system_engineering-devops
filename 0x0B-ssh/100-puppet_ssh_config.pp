31dfc14e421b52b2074ff551294690e7c3401c66
Munyoro MD <munyoro.md95@gmail.com>
2023-01-18 11:37:27 +0200

    h

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,7 @@ include stdlib
 file_line { 'SSH Private Key':
   path               => '/etc/ssh/ssh_config',
   line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/school$',
   replace            => true,
   append_on_no_match => true
 }
